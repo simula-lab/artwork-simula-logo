ac1bdb06870a4780d968bf5f12a5f0f68529f248
Oussama Jarrousse <oussama@jarrousse.org>
2023-11-23 16:18:08 +0100

    added examples

---
+++ python-scripts/examples/example2.py
@@ -0,0 +1,40 @@
+# example2.py
+import math
+
+from penrose import BtileL
+from penrose import BtileS
+from penrose import PenroseP3
+
+# A "sun", with randomly-coloured tiles and including arc paths
+
+scale = 100
+config = {
+    "draw-arcs": True,
+    "tile-opacity": 0.6,
+    "random-tile-colours": True,
+    "Aarc-colour": "#f44",
+    "Carc-colour": "#44f",
+}
+tiling = PenroseP3(scale, ngen=4, config=config)
+
+theta = math.pi / 5
+alpha = math.cos(theta)
+rot = math.cos(theta) + 1j * math.sin(theta)
+A1 = scale + 0.0j
+B = 0 + 0j
+C1 = C2 = A1 * rot
+A2 = A3 = C1 * rot
+C3 = C4 = A3 * rot
+A4 = A5 = C4 * rot
+C5 = -A1
+tiling.set_initial_tiles(
+    [
+        BtileS(A1, B, C1),
+        BtileS(A2, B, C2),
+        BtileS(A3, B, C3),
+        BtileS(A4, B, C4),
+        BtileS(A5, B, C5),
+    ]
+)
+tiling.make_tiling()
+tiling.write_svg("example2.svg")
---
+++ python-scripts/examples/example3.py
@@ -0,0 +1,40 @@
+# example3.py
+import math
+
+from penrose import BtileL
+from penrose import PenroseP3
+from penrose import psi
+
+# A star with five-fold symmetry
+
+# The Golden ratio
+phi = 1 / psi
+scale = 100
+config = {
+    "draw-arcs": True,
+    "Aarc-colour": "#ff5e25",
+    "Carc-colour": "none",
+    "Stile-colour": "#090",
+    "Ltile-colour": "#9f3",
+    "rotate": math.pi / 2,
+}
+tiling = PenroseP3(scale * 2, ngen=5, config=config)
+
+theta = 2 * math.pi / 5
+rot = math.cos(theta) + 1j * math.sin(theta)
+
+B1 = scale
+p = B1 * rot
+q = p * rot
+
+C5 = -scale * phi
+r = C5 / rot
+s = r / rot
+A = [0] * 5
+B = [scale, p, p, q, q]
+C = [s, s, r, r, C5]
+
+tiling.set_initial_tiles([BtileL(*v) for v in zip(A, B, C)])
+tiling.make_tiling()
+
+tiling.write_svg("example3.svg")
